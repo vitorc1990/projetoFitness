d65120ffa6fe8613ef7d64a68390601e57d4975e
Vitor Cabral de Oliveira <vitorc1990@gmail.com>
2022-11-08 16:47:40 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/homePage.Html");
+})
+
+app.listen(4000, function(error) {
+    if (error) {
+        console.log("Erro ao iniciar")
+    }else {
+        console.log(`App exemplo escutando na porta http://localhost:${4000}/`);
+    }
+});

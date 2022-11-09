05c822c1f1bddc47d09d81c2b1b0df057786f613
Vitor Cabral de Oliveira <vitorc1990@gmail.com>
2022-11-09 17:36:50 -0300

    Sexto commit, com segunda funcao

--- public/funcoes.js
+++ public/funcoes.js
@@ -4,3 +4,19 @@ function calculaAgua() {
     document.write(` Você precisa ingerir ${result.toFixed(2)} litros de água por dia`);
     console.log(result.toFixed(2));
 }
+
+function calculaGet() {
+    var masc = document.getElementById("input_homem").value;
+    var fem = document.getElementById("input_mulher").value;
+    var peso = document.getElementById("input_calc_peso").value;
+    var altura = document.getElementById("input_calc_altura").value;
+    var idade = document.getElementById("input_calc_idade").value;
+    var atividade = document.getElementById("input_sedentario").value;
+    if (masc == "homem") {
+        var result = 66 + (13.7 * peso) + (5 * altura) - (6.8 * idade);
+        document.write(`Seu Gasto Energético Total é ${result * atividade} Kcal.`)
+    } else if (fem == "mulher") {
+        var result = 655 + (9.6 * peso) + (1.8 * altura) - (4.7 * idade);
+        document.write(`Seu Gasto Energético Total é ${result * atividade} Kcal.`)
+    }
+}
